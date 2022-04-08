8379053d9c76588e2464aa34d31fd2652ff95845
26Saksham <95184568+26Saksham@users.noreply.github.com>
2022-04-09 01:12:45 +0530

    Add files via upload

---
+++ pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import image from './del.jpg';
+const Home = () => {
+  return (    
+     <div>
+        <h1>Welcome to our website!</h1>
+      <img src={image} height="100%" width="100%"></img> 
+     </div>
+  );
+};
+
+export default Home;
