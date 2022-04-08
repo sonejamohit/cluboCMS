b2ff92edd42a90f0440c5c89344bec2e16c06e07
26Saksham <95184568+26Saksham@users.noreply.github.com>
2022-04-09 01:25:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from './pages/home';
+import About from './pages/about';
+import Login from './pages/login';
+import Contact from './pages/contact';
+const App = () => {
+
+  
+
+  return (
+    <div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/"  element={<Home/>} />
+        <Route  path="/about"  element={<About/>} />
+        <Route  path="/login"  element={<Login/>} />
+        <Route exact path="/contact"  element={<Contact/>} />
+
+      </Routes>
+    </Router>
+     </div>
+     
+  );
+};
+
+export default App;
---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React  from "react";
+
+const Contact=()=>{
+    return(
+    <div>
+<hi>Conatct pages</hi>
+</div>
+)
+}
+export default Contact;
